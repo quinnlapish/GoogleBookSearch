636771041ce32f4ba532f8c4a7223b7b3574ffe4
Quinn Lapish <quinn.lapish99@gmail.com>
2019-09-11 14:41:23 -0400

    tunafish

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-
+const PORT = process.env.PORT || 8080;
 app.use(express.static('client/build'));
 
 
-app.listen(8080, () =>
+app.listen(PORT, () =>
   console.log('Express server is running on localhost:8080')
 );
